16931a3a57a257a0007033eb76dab6781eb99f8b
netaniger <netanel@niger.com>
2025-02-11 15:49:11 +0200

    feat(bariables.tf): add vm credentials and user id

--- variables.tf
+++ variables.tf
@@ -10,10 +10,25 @@ variable "location" {
 
 variable "vnet_vm_name" {
   description = "Name of vnet vm"
-  default     = "vnet_vm"
+  default     = "vnet-vm"
 }
 
 variable "vnet_func_app_name" {
   description = "Name of vnet function app"
-  default     = "vnet_function_app"
+  default     = "vnet-function-app"
+}
+
+variable "user_principal_id" {
+  description = "Allows access to VM via RDP"
+  default = "db506c9c-457c-4d86-81dd-6e5b2670c7be"
+}
+
+variable "admin_username" {
+  description = "Admin username"
+  default = "oriram"
+}
+
+variable "admin_password" {
+  description = "Admin password"
+  default = "Homogadol123"
 }
