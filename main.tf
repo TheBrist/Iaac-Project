8b4d90f8d5feafc009f14df9c0ca68edd6a3282b
netaniger <netanel@niger.com>
2025-02-11 16:20:01 +0200

    feat(main.tf): add function app with private endpoint

--- main.tf
+++ main.tf
@@ -16,15 +16,15 @@ locals {
 
   subnets = {
     "subnet-vm" = {
-      address = "10.0.0.0/24"
+      address   = "10.0.0.0/24"
       vnet_name = "${var.vnet_vm_name}"
     }
     "AzureBastionSubnet" = {
-      address = "10.0.1.0/26"
+      address   = "10.0.1.0/26"
       vnet_name = "${var.vnet_vm_name}"
     }
     "subnet-function-app" = {
-      address = "10.1.0.0/24"
+      address   = "10.1.0.0/24"
       vnet_name = "${var.vnet_func_app_name}"
     }
   }
@@ -42,13 +42,13 @@ locals {
 }
 
 resource "azurerm_subnet" "subnet" {
-  for_each = local.subnets
-  name = each.key
-  resource_group_name = var.resource_group_name
+  for_each             = local.subnets
+  name                 = each.key
+  resource_group_name  = var.resource_group_name
   virtual_network_name = each.value.vnet_name
-  address_prefixes = [ each.value.address ]
+  address_prefixes     = [each.value.address]
 
-  depends_on = [ azurerm_virtual_network.vnets ]
+  depends_on = [azurerm_virtual_network.vnets]
 }
 
 resource "azurerm_virtual_network" "vnets" {
@@ -87,7 +87,7 @@ resource "azurerm_network_security_group" "nsg_vm" {
     destination_address_prefix = "*"
   }
 
-    security_rule {
+  security_rule {
     name                       = "Deny-All"
     priority                   = 200
     direction                  = "Inbound"
@@ -104,7 +104,7 @@ resource "azurerm_network_interface" "vm_nic" {
   name                = "vm-nic"
   location            = var.location
   resource_group_name = var.resource_group_name
-  
+
   ip_configuration {
     private_ip_address_allocation = "Dynamic"
     name                          = "internal"
@@ -113,7 +113,7 @@ resource "azurerm_network_interface" "vm_nic" {
 }
 
 resource "azurerm_network_interface_security_group_association" "add_nsg_to_vm_nic" {
-  network_interface_id = azurerm_network_interface.vm_nic.id
+  network_interface_id      = azurerm_network_interface.vm_nic.id
   network_security_group_id = azurerm_network_security_group.nsg_vm.id
 }
 
@@ -155,21 +155,91 @@ resource "azurerm_virtual_machine" "vm" {
 }
 
 resource "azurerm_public_ip" "bastion_public_ip" {
-  name = "bastion-ip"
-  location = var.location
+  name                = "bastion-ip"
+  location            = var.location
   resource_group_name = var.resource_group_name
-  allocation_method = "Static"
-  sku = "Standard"
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 resource "azurerm_bastion_host" "bastion" {
-  name = "bastion-host"
-  location = var.location
+  name                = "bastion-host"
+  location            = var.location
   resource_group_name = var.resource_group_name
 
   ip_configuration {
-    name = "config"
-    subnet_id = azurerm_subnet.subnet["AzureBastionSubnet"].id
+    name                 = "config"
+    subnet_id            = azurerm_subnet.subnet["AzureBastionSubnet"].id
     public_ip_address_id = azurerm_public_ip.bastion_public_ip.id
+  }
+}
+
+resource "azurerm_storage_account" "function_app_sa" {
+  name = "windows-function-app-sa"
+  resource_group_name = var.resource_group_name
+  location = var.location
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+}
+resource "azurerm_service_plan" "function_app_sp" {
+  name                = "app-service-plan"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  os_type             = "Windows"
+  sku_name            = "Y1"
+}
+
+resource "azurerm_windows_function_app" "function_app" {
+  name                = "windows-function-app"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  service_plan_id = azurerm_service_plan.function_app_sp.id
+  storage_account_name = azurerm_storage_account.function_app_sa.name
+
+  virtual_network_subnet_id = azurerm_subnet.subnet["subnet-function-app"].subnet_id
+  storage_account_access_key = azurerm_storage_account.function_app_sa.primary_access_key
+
+  site_config {
+    vnet_route_all_enabled = true
+  }
+}
+
+resource "azurerm_private_endpoint" "function_app_endpoint" {
+  name = "function-app-private-endpoint"
+  location = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id = azurerm_subnet.subnet["subnet-function-app"].subnet_id
+
+  private_service_connection {
+    name = "function-app-connection"
+    private_connection_resource_id = azurerm_windows_function_app.function_app.id
+    subresource_names = [ "sites" ]
+    is_manual_connection = false
+  }
+}
+
+resource "azurerm_private_dns_zone" "function_app_dns" {
+  name = "privatelink.azurewebsites.net"
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "dns_link" {
+  name = "function-app-dns-link"
+  resource_group_name = var.resource_group_name
+  private_dns_zone_name = azurerm_private_dns_zone.function_app_dns.name
+  virtual_network_id = azurerm_virtual_network.vnets["${var.vnet_func_app_name}"].id
+}
+
+resource "azurerm_private_endpoint" "storage_endpoint" {
+  name = "sa-private-endpoint"
+  location = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id = azurerm_subnet.subnet["subnet-function-app"].subnet_id
+  private_service_connection {
+    name = "storage-account-connection"
+    private_connection_resource_id = azurerm_storage_account.function_app_sa.id
+    subresource_names = [ "blob" ]
+    is_manual_connection = false
   }
 }
