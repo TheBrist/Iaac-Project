ae6729571962760956f7a759632286bd7f593cd7
netaniger <netanel@niger.com>
2025-02-24 10:56:31 +0200

    feat(main.tf): add cloud run with storage admin role

--- gcp/main.tf
+++ gcp/main.tf
@@ -4,7 +4,7 @@ provider "google" {
 module "vpc" {
   source     = "./modules/net-vpc"
   project_id = var.project_id_01
-  name       = "my-network"
+  name       = "mynetwork"
   subnets = [
     {
       ip_cidr_range = "10.0.0.0/24"
@@ -19,41 +19,40 @@ module "vpc" {
   ]
 }
 
-# module "cloud_run" {
-#   source = "./fabric/modules/cloud-run-v2"
-#   project_id = var.project_id_01
-#   region = var.region
-#   name = "cloudRun"
-#   containers = {
-#     storage_api = {
-#       image = "gcr.io/google.com/cloudsdktool/cloud-sdl:slim"
+module "cloud_run" {
+  source = "./modules/cloud-run-v2"
+  project_id = var.project_id_01
+  region = var.region
+  name = "cloudrun123"
+  containers = {
+    storage-api = {
+      image = "us-docker.pkg.dev/cloudrun/container/hello"
+    }
+  }
 
-#     }
-#   }
+  ingress = "INGRESS_TRAFFIC_INTERNAL_LOAD_BALANCER"
 
-#   ingress = "internal-and-cloud-load-balancing"
+  vpc_connector_create = {
+    ip_cidr_range = "10.2.0.0/28"
+    network = module.vpc.self_link
+    instances = {
+      max = 10
+      min = 3
+    }
+  } 
 
-#   vpc_connector_create = {
-#     ip_cidr_range = "10.2.0.0/28"
-#     network = module.vpc.self_link
-#     instances = {
-#       max = 10
-#       min = 3
-#     }
-#   } 
+  service_account = module.cloud_run_sa.email
+  deletion_protection = false
+}
 
-#   service_account = module.cloud_run_sa.email
-#   deletion_protection = false
-# }
+module "cloud_run_sa" {
+  source     = "./modules/iam-service-account"
+  project_id = var.project_id_01
+  name = "cloudrunsa"
 
-# module "cloud_run_sa" {
-#   source     = "./fabric/modules/iam-service-account"
-#   project_id = var.project_id_01
-#   name = "cloud-run-sa"
-
-#   iam_project_roles = {
-#     var.project_id_01 = [
-#       "roles/storage.objectAdmin"
-#     ]
-#   }
-# }
+  iam_project_roles = {
+    "${var.project_id_01}" = [
+      "roles/storage.objectAdmin"
+    ]
+  }
+}
