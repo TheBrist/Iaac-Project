a3978517e47cb9cd6b7ed812f191b7776f45c07f
netaniger <netanel@niger.com>
2025-02-24 15:50:38 +0200

    feat(variables.tf): add subnets

--- gcp/variables.tf
+++ gcp/variables.tf
@@ -9,3 +9,25 @@ variable "project_id_02" {
 variable "region" {
   default = "me-west1"
 }
+
+
+variable "subnets" {
+  default = {
+    "psc-projects" = {
+      ip_cidr_range = "10.0.0.0/24"
+      name          = "psc-projects"
+    },
+    "ilb" = {
+      ip_cidr_range = "10.1.0.0/24"
+      name          = "ilb"
+    },
+    "cr-vpc-connector" = {
+      ip_cidr_range = "10.2.0.0/28"
+      name          = "cr-vpc-connector"
+    },
+    "psc-cr" = {
+      ip_cidr_range = "10.3.0.0/24"
+      name          = "psc-cr"
+    }
+  }
+}
