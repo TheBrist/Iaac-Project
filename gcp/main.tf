6111d40abc084f6777716520f21d82a8701c5672
netaniger <netanel@niger.com>
2025-02-27 17:32:48 +0200

    main.tf: use ilb-l7 module with neg group as BE for cloudrun

--- gcp/main.tf
+++ gcp/main.tf
@@ -1,29 +1,32 @@
 provider "google" {
   region = var.region
 }
+
 module "vpc" {
   source     = "./modules/net-vpc"
   project_id = var.project_id_01
   name       = "mynetwork"
-  subnets = [
+
+  subnets = [for subnet in var.subnets : {
+    ip_cidr_range = subnet.ip_cidr_range
+    name          = subnet.name
+    region        = var.region
+  }]
+
+  subnets_proxy_only = [
     {
-      ip_cidr_range = "10.0.0.0/24"
-      name          = "psc"
-      region        = var.region
-    },
-    {
-      ip_cidr_range = "10.1.0.0/24"
-      name          = "ilb"
+      ip_cidr_range = "10.10.0.0/24"
+      name          = "proxy-only-subnet"
       region        = var.region
     }
   ]
 }
 
 module "cloud_run" {
-  source = "./modules/cloud-run-v2"
+  source     = "./modules/cloud-run-v2"
   project_id = var.project_id_01
-  region = var.region
-  name = "cloudrun123"
+  region     = var.region
+  name       = "cloudrun123"
   containers = {
     storage-api = {
       image = "us-docker.pkg.dev/cloudrun/container/hello"
@@ -33,26 +36,66 @@ module "cloud_run" {
   ingress = "INGRESS_TRAFFIC_INTERNAL_LOAD_BALANCER"
 
   vpc_connector_create = {
-    ip_cidr_range = "10.2.0.0/28"
-    network = module.vpc.self_link
+    ip_cidr_range = "10.5.0.16/28"
+    network       = module.vpc.self_link
     instances = {
       max = 10
       min = 3
     }
-  } 
+  }
 
-  service_account = module.cloud_run_sa.email
+  service_account     = module.cloud_run_sa.email
   deletion_protection = false
 }
 
 module "cloud_run_sa" {
   source     = "./modules/iam-service-account"
   project_id = var.project_id_01
-  name = "cloudrunsa"
+  name       = "cloudrunsa"
 
   iam_project_roles = {
     "${var.project_id_01}" = [
       "roles/storage.objectAdmin"
     ]
+  }
+}
+
+module "ilb-l7" {
+  source     = "./modules/net-lb-app-int"
+  name       = "internal-lb"
+  project_id = var.project_id_01
+  region     = var.region
+  backend_service_configs = {
+    default = {
+      backends = [{
+        group = "my-neg"
+      }]
+      health_checks = []
+    }
+  }
+  health_check_configs = {
+    health-check = {
+      check_interval_sec  = 10
+      timeout_sec         = 5
+      healthy_threshold   = 2
+      unhealthy_threshold = 2
+      name                = "health-check"
+      http = {
+        port = 80
+      }
+  } }
+  neg_configs = {
+    my-neg = {
+      cloudrun = {
+        region = var.region
+        target_service = {
+          name = "cloudrun123"
+        }
+      }
+    }
+  }
+  vpc_config = {
+    network    = module.vpc.self_link
+    subnetwork = module.vpc.subnets["${var.region}/ilb"].self_link
   }
 }
